b2f48c7a88bc0a9f005ae9382e2c86534c34790a
nacholiu <nacholiu13@gmail.com>
2025-04-23 23:21:36 -0300

    cambiar variables y agregar comentarios, implementacion principal y su movimiento, --hacer doctring y funciones donde aqui se puede  aplicar--

--- main.py
+++ main.py
@@ -11,33 +11,33 @@ pantalla = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption("Space Invader 🚀")
 
 # Colores
-NEGRO = (0, 0, 0) #Fondo
-BLANCO = (255, 255, 255) #Bala
+NEGRO = (0, 0, 0) #Fondo pantalla
+BLANCO = (255, 255, 255) #Bala de la nave
 
 # Jugador
 nave_img = pygame.Surface((50, 10)) # Cambia el tamaño del jugador
 nave_img.fill((0, 255, 0))
 Poscion_nave_x = ANCHO // 2
 Poscion_nave_y = ALTO - 10
-velocidad = 5
+velocidad_nave = 5
 
 # Bucle principal
 reloj = pygame.time.Clock()
-movimiento = True
+movimiento_lateral = True
 
-while movimiento:
+while movimiento_lateral:
     reloj.tick(60)  # 60 FPS
 
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
-            movimiento = False
+            movimiento_lateral = False
 
     # Teclas para mover el jugador
     teclas = pygame.key.get_pressed()
     if teclas[pygame.K_LEFT] and Poscion_nave_x > 0:
-        Poscion_nave_x -= velocidad
+        Poscion_nave_x -= velocidad_nave
     if teclas[pygame.K_RIGHT] and Poscion_nave_x < ANCHO - 50:
-        Poscion_nave_x += velocidad
+        Poscion_nave_x += velocidad_nave
 
     # Dibujar fondo y jugador
     pantalla.fill(NEGRO)
